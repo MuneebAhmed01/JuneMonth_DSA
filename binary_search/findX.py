e3063146db006c0723cd5327c7520e22c6dc8ca8
MuneebAhmed01 <imuneebahmed0@gmail.com>
2025-06-01 19:18:55 +0500

    Binary search Easy Practice

---
+++ binary_search/findX.py
@@ -0,0 +1,23 @@
+# Binary Search to find X in sorted array X is let say 6
+
+def binary(arr,target):
+    left=0
+    right=len(arr)-1
+    while left<right:
+        mid = (left + right) // 2
+
+        if arr[mid] == target:
+            return mid  # Target found
+
+        elif arr[mid] < target:
+            left = mid + 1  # Search in the right half
+
+        else:
+            right = mid - 1 
+
+arr=[0,1,2,3,4,5,6,7,8,9]
+target=7
+print(binary(arr,target))
+    
+
+
