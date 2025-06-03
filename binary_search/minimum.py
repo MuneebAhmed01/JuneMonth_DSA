b95df971f24a3ae290de3338e15f2dd0417ec968
MuneebAhmed01 <imuneebahmed0@gmail.com>
2025-06-03 20:41:28 +0500

    2 new questions

---
+++ binary_search/minimum.py
@@ -0,0 +1,27 @@
+
+def small(arr):
+    start=0
+    end=len(arr)-1
+    while start<=end:
+        mid=(start+end)//2
+        if arr[mid]<arr[end]:
+            res=arr[mid]
+            end=mid-1
+        else:
+            start=mid+1
+            res=arr[end]
+    return res
+
+
+
+
+
+
+
+
+arr = [4, 5, 6, 7, 0, 1, 2]
+
+
+
+
+print(small(arr))
---
+++ binary_search/rotation.py
@@ -0,0 +1,16 @@
+def rotate(arr):
+    start=0
+    end=len(arr)-1
+    while start<end:
+        mid=(start+end)//2
+        if arr[start]<arr[mid]:
+            res=start
+            start=mid
+        else:
+            res=mid
+            end=mid
+    return res+1
+
+
+arr=[122,123,124,125,134,136,145,167,183,56,76,87,98,102,103,119]
+print(rotate(arr))
